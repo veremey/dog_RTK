77c48fb3fdfe594576c1c8e29d3b06bf265a820d
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-04-28 18:12:55 +0200

    api - createApi

---
+++ src/store/apiSlice.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api =createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  endpoints: (builder) => ({}),
+})
+ 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
+import { api } from './apiSlice';
 import { configureStore } from "@reduxjs/toolkit";
-import servicesReducer from "../pages/services/servicesSlice";
 import dogsReducer from "../pages/dogs/dogsSlice";
+import servicesReducer from "../pages/services/servicesSlice";
 
 export const store = configureStore({
   reducer: {
     dogs: dogsReducer,
     services: servicesReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(api.middleware)
+  
 });
