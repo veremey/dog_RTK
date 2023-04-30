64d87ec1b8859d0f4444c58ae3620ced25f8d2e9
Sergey Veremiychuk <veremey4uk@gmail.com>
2023-04-30 22:56:36 +0200

    Caching with transformResponce

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,11 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getAge, getSize } from '../pages/dogs/dogsSlice';
 
 export const api =  createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
   endpoints: (builder) => ({
     getServices: builder.query({query: () => "/services",}),
     getService: builder.query({query: (id) => `/services/${id}`}),
-    getDogs: builder.query({query: () => '/dogs'}),
+    getDogs: builder.query({
+      query: () => '/dogs',
+      transformResponse: (dogs) => {
+        const allDogs = {};
+        for (const id in dogs) {
+          const dog = dogs[id];
+          allDogs[id] = {
+            ...dog,
+            size: getSize(dog.weight),
+            age: getAge(dog.dob)
+          }
+        }
+      }
+    }
+    
+    ),
     makeContact: builder.mutation({
       query: (body) => ({
         url: "contact", 
